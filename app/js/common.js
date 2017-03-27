373904e89f38faf6842d02c4e4e99ce5b5036308
@Scriptus <zonder2009@gmail.com>
2017-03-27 22:16:07 +0300

    секция [Аккордеон]

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,28 @@
 $(function() {
 
+	/* TABS
+	============= */
 	$('#tab-all').easytabs();
+
+	/* ACCORDEON
+	============== */	
+	$(".link-js").on("click", function(e) {
+
+		e.preventDefault();
+		var $this = $(this);
+
+        if( !$this.hasClass("link-js__active") ) {
+            $(".description-js").slideUp();
+            $(".link-js").removeClass("link-js__active");
+        }
+
+        $this.toggleClass("link-js__active");
+        $this.next().slideToggle();
+
+
+	});
+
+
 
 });
 
