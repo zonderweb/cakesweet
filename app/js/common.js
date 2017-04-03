96ffab528c2b78dcf056664ab4048e2861c4a5ab
@Scriptus <zonder2009@gmail.com>
2017-04-03 21:25:59 +0300

    slide mmenu + начало адаптивности

--- app/js/common.js
+++ app/js/common.js
@@ -2,7 +2,33 @@ $(function() {
 
 	/* mmenu
 	============= */
-	$('#my-menu').mmenu(); 
+	$('#my-menu').mmenu({
+
+		"offCanvas": {
+			"position": "right",
+			"pageSelector": "#all-wrapper"
+		},
+
+		"extensions": [
+			"pagedim-black", "fx-menu-fade"
+		],
+
+		"navbar": {
+			"title": '<img src="img/logo.png" alt="Cakesweet">'
+		}
+
+	});
+
+	var api = $('#my-menu').data('mmenu'); 
+
+	api.bind('open:start', function() {
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind('close:before', function() {
+		$('.hamburger').removeClass('is-active');
+	});
+
+	
 
 	/* TABS
 	============= */
